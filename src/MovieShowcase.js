c92649322a6fcc3fbc7055cbd8f60cd8f6cc3b21
patrick-rush <patrick.a.rush@gmail.com>
2021-02-01 20:51:36 -0500

    commit tree ec852874786b211d2eb28dcb654eff35f77e650c

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,10 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map(movie => {
+      // console.log(movie)
+      return <MovieCard title={movie.title} IMDBRating={movie.IMDBRating} genres={movie.genres} poster={movie.poster} />
+  })
   }
 
   render() {
